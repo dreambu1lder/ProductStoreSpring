0574df852bf91c5f80ad41156526d36f3d82c818
dreambu1lder <ctukcuk@mail.ru>
2024-09-20 17:04:10 +0700

    Bug fixes

--- src/main/test/java/productstore/dao/utils/PostgreSQLContainerProvider.java
+++ src/main/test/java/productstore/dao/utils/PostgreSQLContainerProvider.java
@@ -6,6 +6,9 @@ import org.testcontainers.containers.output.Slf4jLogConsumer;
 import org.testcontainers.containers.wait.strategy.Wait;
 import productstore.db.DataBaseUtil;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -41,38 +44,22 @@ public class PostgreSQLContainerProvider {
         }
     }
 
-    
+
     public static void initializeDatabase() {
+        String schemaFilePath = "docker/init/schema.sql";
+
         try (Connection connection = DataBaseUtil.getConnection();
              Statement stmt = connection.createStatement()) {
 
-            
-            stmt.execute("CREATE TABLE IF NOT EXISTS users (" +
-                    "id BIGSERIAL PRIMARY KEY, " +
-                    "name VARCHAR(255) UNIQUE NOT NULL CHECK (trim(name) <> ''), " +
-                    "email VARCHAR(255) NOT NULL CHECK (trim(email) <> '')" +
-                    ");");
+            String sqlScript = new String(Files.readAllBytes(Paths.get(schemaFilePath)));
 
-            stmt.execute("CREATE TABLE IF NOT EXISTS products (" +
-                    "id BIGSERIAL PRIMARY KEY, " +
-                    "name VARCHAR(255) NOT NULL CHECK (trim(name) <> ''), " +
-                    "price DECIMAL(10, 2) NOT NULL CHECK (price >= 0)" +
-                    ");");
+            for (String sqlCommand : sqlScript.split(";")) {
+                if (!sqlCommand.trim().isEmpty()) {
+                    stmt.execute(sqlCommand);
+                }
+            }
 
-            stmt.execute("CREATE TABLE IF NOT EXISTS orders (" +
-                    "id BIGSERIAL PRIMARY KEY, " +
-                    "user_id BIGINT NOT NULL, " +
-                    "CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE" +
-                    ");");
-
-            stmt.execute("CREATE TABLE IF NOT EXISTS orders_products (" +
-                    "order_id BIGINT NOT NULL, " +
-                    "product_id BIGINT NOT NULL, " +
-                    "PRIMARY KEY (order_id, product_id), " +
-                    "CONSTRAINT fk_order FOREIGN KEY (order_id) REFERENCES orders(id) ON DELETE CASCADE, " +
-                    "CONSTRAINT fk_product FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE" +
-                    ");");
-        } catch (SQLException e) {
+        } catch (IOException | SQLException e) {
             e.printStackTrace();
             throw new RuntimeException("Failed to initialize the database", e);
         }
